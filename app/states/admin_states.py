e8cc9dcafce0a7d5329358684c4d6a1c693325b8
Soheilecho <121904056+soheilechoo@users.noreply.github.com>
2026-09-09 17:55:58 +0330

    Update admin_states.py

---
+++ app/states/admin_states.py
@@ -0,0 +1,15 @@
+from aiogram.fsm.state import State, StatesGroup
+
+class AdminState(StatesGroup):
+    # مدیریت قیمت‌ها
+    waiting_for_product_selection = State()
+    waiting_for_new_price = State()
+    
+    # مدیریت کارت‌های بانکی
+    waiting_for_card_number = State()
+    waiting_for_card_holder = State()
+    confirm_card = State()
+    
+    # مدیریت کاربران
+    waiting_for_user_search = State()
+    waiting_for_block_reason = State()
